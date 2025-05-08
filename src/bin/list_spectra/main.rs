60a00811318accc4405cc5451179ca9693d84d11
Joshua Laughner <josh.laughner@jpl.nasa.gov>
2025-05-08 14:43:57 -0700

    Added option to expand glob patterns internally in `list_spectra`

--- src/bin/list_spectra/main.rs
+++ src/bin/list_spectra/main.rs
@@ -3,10 +3,20 @@ use std::path::PathBuf;
 use clap::Parser;
 
 use ggg_rs::tccon::sort_spectra;
+use glob::glob;
+use itertools::Itertools;
 
 fn main() {
     let args = Cli::parse();
-    let spectra_names = sort_spectra::sort_spectrum_names(&args.spectra)
+    let spectra = if args.expand_globs {
+        expand_glob_patterns(&args.spectra).expect("Could not parse or match glob patterns")
+    } else {
+        args.spectra
+            .into_iter()
+            .map(|s| PathBuf::from(s))
+            .collect_vec()
+    };
+    let spectra_names = sort_spectra::sort_spectrum_names(&spectra)
         .expect("Was not able to extract the base names of all given spectra");
 
     for name in spectra_names {
@@ -39,5 +49,29 @@ fn main() {
 struct Cli {
     /// The spectra to print in order. May be full paths to spectra, only the names
     /// will be printed.
-    spectra: Vec<PathBuf>,
+    spectra: Vec<String>,
+
+    /// If glob patterns (.e.g, *, ?) are given as arguments, expand them before
+    /// trying to list the files. If not given, then it is assumed the shell will
+    /// have expanded such wildcards. If you need to avoid the shell expanding the
+    /// glob patterns (because you are reaching the limit of the number of command
+    /// line arguments allowed), you will need to quote any glob patterns.
+    #[clap(short = 'g', long)]
+    expand_globs: bool,
+}
+
+fn expand_glob_patterns(patterns: &[String]) -> Result<Vec<PathBuf>, String> {
+    let mut spectra = vec![];
+
+    for pat in patterns {
+        let matches = glob(pat).map_err(|e| e.to_string())?;
+        for p in matches {
+            match p {
+                Ok(p) => spectra.push(p),
+                Err(e) => return Err(e.to_string()),
+            }
+        }
+    }
+
+    Ok(spectra)
 }
